ea267f63781d923b441532af06b068ff2d3b2933
Yagiz Tansu <yagiz.tansu99@gmail.com>
2024-11-16 15:06:34 +0100

    DELETE : build folder deleted

--- build/ext/csv-parser/include/internal/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yagiz/Desktop/OptiWise/ext/csv-parser/include/internal
-# Build directory: /home/yagiz/Desktop/OptiWise/build/ext/csv-parser/include/internal
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/ext/csv-parser/programs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yagiz/Desktop/OptiWise/ext/csv-parser/programs
-# Build directory: /home/yagiz/Desktop/OptiWise/build/ext/csv-parser/programs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
