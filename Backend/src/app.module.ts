077a171f3fdcf8c97519976eaf5c02314b0a3cb5
Yagiz Tansu <yagiz.tansu99@gmail.com>
2025-03-22 15:15:32 +0300

    Remove CotModule from AppModule: eliminate unused module from imports

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FinanceModule } from './finance/finance.module';
-import { CotModule } from './cot/cot.module';
 
 @Module({
-  imports: [FinanceModule, CotModule],
+  imports: [FinanceModule],
   controllers: [AppController],
   providers: [AppService],
 })
